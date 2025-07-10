65c1903406a1f78cbab8346312cbbf7a12c8fdc2
Cheshy <celiarico@hotmail.com>
2025-07-10 23:51:31 +0200

    Unused vars error

--- scripts/common.sh
+++ scripts/common.sh
@@ -5,8 +5,12 @@ set -euo pipefail
 current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # Ruta de carpetas internas
+
+# shellcheck disable=SC2034
 scripts_path="$current_path/scripts"
+# shellcheck disable=SC2034
 config_path="$current_path/config"
+# shellcheck disable=SC2034
 resources_path="$current_path/resources"
 backup_path="$current_path/backup"
 logs_path="$current_path/logs"
